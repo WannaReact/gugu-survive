ce8fd7ee538bcce3eda4b13a9ab6e96e6176fb5b
정민킴 <jma1020@naver.com>
2022-01-28 00:28:17 +0900

    :sparkles: GameKeyPad 컴포넌트 구현

--- src/components/game/GameKeyPad.jsx
+++ src/components/game/GameKeyPad.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import propTypes from 'prop-types';
 import Button from '../Button';
 import SPEC from '../../constants/spec';
 import COLOR from '../../constants/color';
 
 const KeyPad = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(3, 60px);
   gap: 13px;
-  justify-items: center;
   padding: 0px 92px;
 `;
 
-const GameKeyPad = () => {
+const GameKeyPad = ({ setInp, gameLogic }) => {
   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+  const ttt = (item) => () => {
+    setInp((prev) => prev + item);
+  };
+
+  // onClick={()=>setInp(item)} 하면 되는 줄 알았는데, 다른법 있나? 고차함수말고
 
   return (
     <KeyPad>
       {array.map((item) => {
         return (
-          <Button spec={SPEC.NUM_PAD} color={COLOR.NUM_PAD} key={item}>
+          <Button
+            spec={SPEC.NUM_PAD}
+            color={COLOR.NUM_PAD}
+            key={item}
+            onClick={ttt(item)}
+          >
             {`${item}`}
           </Button>
         );
       })}
+      <Button spec={SPEC.INPUT_BUTTON} color={COLOR.RED} onClick={gameLogic}>
+        입력
+      </Button>
     </KeyPad>
   );
 };
 
-export default GameKeyPad;
+GameKeyPad.propTypes = {
+  setInp: propTypes.func,
+  gameLogic: propTypes.func
+};
+
+export default React.memo(GameKeyPad);

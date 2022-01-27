0ed4e9e75510080e20bed0730cfdd21fc372d4d7
skgml0 <hnh2714@gmail.com>
2022-01-28 00:18:13 +0900

    ✨  modal size 상수 추가

---
+++ src/constants/modal-spec.js
@@ -0,0 +1,14 @@
+const MODAL_SPEC = {
+  NAME_MODAL: {
+    MOBILE: { width: '288px', minHeight: '170px' },
+    DESKTOP: { width: '770px', minHeight: '262px' }
+  },
+  EXPLAIN_MODAL: {
+    MOBILE: { width: '288px', maxHeight: '600px' },
+    DESKTOP: { width: '625px', minHeight: '664px' }
+  },
+  OVER_MODAL: {
+    MOBILE: { width: '288px', minHeight: '345px' },
+    DESKTOP: { width: '737px', minHeight: '532px' }
+  }
+};

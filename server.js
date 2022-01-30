1f23bdd0f3a6151f960dfbeeef97f227765dfb6d
Tesseract <tensai3355@gmail.com>
2022-01-30 22:26:05 +0900

    :fire: webpack 관련 코드 삭제

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
+const path = require('path');
 const express = require('express');
-const webpack = require('webpack');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const webpackConfig = require('./webpack.config');
 const addRecordRoute = require('./routes/addRecord');
 const leaderboardRoute = require('./routes/leaderboard');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
-const compiler = webpack(webpackConfig);
-const middleware = webpackDevMiddleware(compiler, {
-  publicPath: webpackConfig.output.publicPath
-});
 
 dotenv.config({ path: 'variables.env' });
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }, (err) => {
@@ -24,24 +17,15 @@ mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }, (err) => {
   }
 });
 
+app.use(express.static(path.resolve(__dirname, './build')));
 app.use(express.json());
-
-app.use(middleware);
-
-app.use(
-  webpackHotMiddleware(compiler, {
-    log: console.log,
-    path: '/__webpack_hmr',
-    heartbeat: 10 * 1000
-  })
-);
 
 app.use('/', addRecordRoute);
 
 app.use('/', leaderboardRoute);
 
-app.use((req, res, next) => {
-  res.status(404).send('잘못된 주소입니다.');
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 
 app.listen(PORT, () => console.log(`👂 listening on ${PORT}`));
